977db07a7041d8a1c8450d0f0b0b0dc540345451
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2025-12-01 19:35:45 +0200

    feat: implemented tick and a way to quicky test in rust, until I get to the WASM part

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::fmt::{Display, Formatter};
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -31,7 +33,7 @@ impl Universe {
     fn get_index(&self, row: u32, col: u32) -> usize {
         (row * self.width + col) as usize
     }
-    
+
     fn num_alive_neighbours(&self, row: u32, col: u32) -> u8 {
         let mut count = 0;
 
@@ -48,5 +50,71 @@ impl Universe {
             }
         }
         return count;
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let i = self.get_index(row, col);
+
+                let cell = self.cells[i];
+                let alive_neighbours = self.num_alive_neighbours(row, col);
+                match (cell, alive_neighbours) {
+                    (Cell::Alive, 0 | 1) => {
+                        //underpopulation
+                        next[i] = Cell::Dead;
+                    }
+                    (Cell::Alive, 2 | 3) => {
+                        //continues to live
+                        next[i] = Cell::Alive;
+                    }
+                    (Cell::Alive, _) => {
+                        //overpopulation
+                        next[i] = Cell::Dead;
+                    }
+                    (Cell::Dead, 3) => {
+                        //reproduction
+                        next[i] = Cell::Alive;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new(width: u32, height: u32) -> Self{
+        // set the middle cell to 1
+        let mut cells   = vec![Cell::Dead; (width*height) as usize];
+        let i = (((width * height) / 2) + width/2) as usize;
+        cells[i-1] = Cell::Alive;
+        cells[i] = Cell::Alive;
+        cells[i+1] = Cell::Alive;
+        Self {width: width, height: height, cells: cells}
+    }
+}
+
+impl Display for Universe {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let i = self.get_index(row, col);
+
+                let cell = self.cells[i];
+                match cell {
+                    Cell::Alive => write!(f, "1 "),
+                    Cell::Dead => write!(f, "0 "),
+                }?
+            }
+            writeln!(f, "")?;
+        }
+
+        Ok(())
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use wasm_game_of_life::Universe;
+
+fn main()
+{
+    let mut universe: Universe = Universe::new(10, 10);
+    println!("{}", universe);
+
+    for i in 0..100{
+        universe.tick();
+    }
+    println!("{}", universe);
+
+}
